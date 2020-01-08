126b244ff4e0ee3644fc273c09129736cb260fb1
jonahbrennan <jonahbrennan@gmail.com>
2020-01-07 23:10:35 -0800

    first try

---
+++ Frontend/initdb.py
@@ -0,0 +1,4 @@
+from nba_games.app import db
+
+# db.drop_all()
+db.create_all()
---
+++ Frontend/nba_games/app.py
@@ -0,0 +1,107 @@
+import os
+
+import pandas as pd
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+#################################################
+# Database Setup
+#################################################
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/nba_data.sqlite"
+db = SQLAlchemy(app)
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(db.engine, reflect=True)
+
+# Save references to each table
+Samples_Metadata = Base.classes.clean_teams
+# Samples = Base.classes.samples
+
+
+@app.route("/")
+def index():
+    """Return the homepage."""
+    return render_template("index.html")
+
+
+@app.route("/names")
+def names():
+    """Return a list of sample names."""
+
+    # Use Pandas to perform the sql query
+    stmt = db.session.query(Samples_Metadata).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
+
+    # Return a list of the column names (sample names)
+    # return jsonify(list(df.columns)[5:])
+    return jsonify(list(df["NICKNAME"]))
+
+
+
+@app.route("/metadata/<NICKNAME>")
+def sample_metadata(NICKNAME):
+    """Return the MetaData for a given TEAM_ID."""
+    sel = [
+        Samples_Metadata.TEAM_ID,
+        # Samples_Metadata.LEAGUE_ID,
+        # Samples_Metadata.MIN_YEAR,
+        # Samples_Metadata.MAX_YEAR,
+        Samples_Metadata.CITY,
+        Samples_Metadata.ABBREVIATION,
+        Samples_Metadata.NICKNAME,
+    ]
+
+    results = db.session.query(*sel).filter(Samples_Metadata.NICKNAME == NICKNAME).all()
+
+    # Create a dictionary entry for each row of metadata information
+    sample_metadata = {}
+    for result in results:
+        sample_metadata["TEAM_ID"] = result[0]
+        # sample_metadata["LEAGUE_ID"] = result[1]
+        # sample_metadata["MIN_YEAR"] = result[2]
+        # sample_metadata["MAX_YEAR"] = result[3]
+        sample_metadata["CITY"] = result[1]
+        sample_metadata["ABBREVIATION"] = result[2]
+        sample_metadata["NICKNAME"] = result[3]
+
+    print(sample_metadata)
+    return jsonify(sample_metadata)
+
+
+# @app.route("/samples/<sample>")
+# def samples(sample):
+#     """Return `otu_ids`, `otu_labels`,and `sample_values`."""
+#     stmt = db.session.query(Samples).statement
+#     df = pd.read_sql_query(stmt, db.session.bind)
+
+#     # Filter the data based on the sample number and
+#     # only keep rows with values above 1
+#     sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
+
+#     # Sort by sample
+#     sample_data.sort_values(by=sample, ascending=False, inplace=True)
+
+#     # Format the data to send as json
+#     data = {
+#         "otu_ids": sample_data.otu_id.values.tolist(),
+#         "sample_values": sample_data[sample].values.tolist(),
+#         "otu_labels": sample_data.otu_label.tolist(),
+#     }
+#     return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run()
