2a82b0bf628561cd01c0e9db74540dc1f37af5a2
jonahbrennan <jonahbrennan@gmail.com>
2020-01-09 10:02:47 -0800

    team logos added grfx updates

--- Frontend/nba_games/static/js/app.js
+++ Frontend/nba_games/static/js/app.js
@@ -5,12 +5,13 @@ function buildMetadataHome(sample) {
 
     // Use d3 to select the panel with id of `#sample-metadata`
     var sample_metadata_tableHome = d3.select("#sample-metadataHome");
-
+    var teamLogo_Home = d3.select("#teamLogo_Home");
     // console.log(sample_metadata_tableHome)
+    // console.log(teamLogo_Home)
 
     // Use `.html("") to clear any existing metadata
     sample_metadata_tableHome.html("");
-
+    teamLogo_Home.html("");
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
@@ -22,35 +23,82 @@ function buildMetadataHome(sample) {
         cell.text(key + ": " + value);
 
       });
+      // Object.entries(data).forEach(([key, value]) => {
+        const obj = Object.entries(data)[0];
+        var div = teamLogo_Home.append("img");
+        div.attr("src", "../static/Images/Logos/" + obj[1] + ".png");
+        div.attr("style", "width:70%" );
+        // console.log(Object.entries(data)[0]); 
+        console.log(obj[1]); 
+
+      // });
+      
     });
     
 }
 
-function buildMetadataAway(sample) {
+// function buildMetadataAway(sample) {
 
-  // @TODO: Complete the following function that builds the metadata panel
+//   // @TODO: Complete the following function that builds the metadata panel
+
+//   // Use `d3.json` to fetch the metadata for a sample
+//   var url = `/metadata/${sample}`;
+
+//     // Use d3 to select the panel with id of `#sample-metadata`
+//     var sample_metadata_tableAway = d3.select("#sample-metadataAway");
+
+//     // console.log(sample_metadata_tableAway)
+
+//     // Use `.html("") to clear any existing metadata
+//     sample_metadata_tableAway.html("");
+
+//     d3.json(url).then((data) => {
+//       Object.entries(data).forEach(([key, value]) => {
+//         var cell = sample_metadata_tableAway.append("li");
+//         cell.text(key + ": " + value);
+
+//       });
+//   });
+    
+// }
+function buildMetadataAway(sample) {
 
   // Use `d3.json` to fetch the metadata for a sample
   var url = `/metadata/${sample}`;
 
     // Use d3 to select the panel with id of `#sample-metadata`
     var sample_metadata_tableAway = d3.select("#sample-metadataAway");
-
-    // console.log(sample_metadata_tableAway)
+    var teamLogo_Away = d3.select("#teamLogo_Away");
+    // console.log(sample_metadata_tableHome)
+    // console.log(teamLogo_Home)
 
     // Use `.html("") to clear any existing metadata
     sample_metadata_tableAway.html("");
+    teamLogo_Away.html("");
+    // Use `Object.entries` to add each key and value pair to the panel
+    // Hint: Inside the loop, you will need to use d3 to append new
+    // tags for each key-value in the metadata.
 
     d3.json(url).then((data) => {
+      // var span = sample_metadata_table.append("span");
       Object.entries(data).forEach(([key, value]) => {
         var cell = sample_metadata_tableAway.append("li");
         cell.text(key + ": " + value);
 
       });
-  });
+      // Object.entries(data).forEach(([key, value]) => {
+        const obj = Object.entries(data)[0];
+        var div = teamLogo_Away.append("img");
+        div.attr("src", "../static/Images/Logos/" + obj[1] + ".png");
+        div.attr("style", "width:70%" );
+        // console.log(Object.entries(data)[0]); 
+        console.log(obj[1]); 
+
+      // });
+      
+    });
     
 }
-
 
 function init() {
   // Grab a reference to the dropdown select element
