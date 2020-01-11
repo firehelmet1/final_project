d5118320092cf1c164f2b399774a5285e463f7b8
Michael Pearson <mpearson2516@yahoo.com>
2020-01-11 11:52:38 -0800

    Fixed the game data route.

--- Frontend/nba_games/app.py
+++ Frontend/nba_games/app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/nba_data.sqlite"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/nba_data2.sqlite"
 db = SQLAlchemy(app)
 
 # reflect an existing database into a new model
@@ -27,8 +27,8 @@ Base = automap_base()
 Base.prepare(db.engine, reflect=True)
 
 # Save references to each table
-Samples_Metadata = Base.classes.clean_teams
-# Samples = Base.classes.samples
+Samples_Metadata = Base.classes.Teams
+Samples = Base.classes.Games
 
 
 @app.route("/")
@@ -53,12 +53,12 @@ def names():
 
 @app.route("/metadata/<NICKNAME>")
 def sample_metadata(NICKNAME):
-    """Return the MetaData for a given TEAM_ID."""
+    """Return the MetaData for a given NICKNAME."""
     sel = [
         Samples_Metadata.TEAM_ID,
         Samples_Metadata.CITY,
         Samples_Metadata.ABBREVIATION,
-        Samples_Metadata.NICKNAME,
+        Samples_Metadata.NICKNAME
     ]
 
     results = db.session.query(*sel).filter(Samples_Metadata.NICKNAME == NICKNAME).all()
@@ -75,26 +75,54 @@ def sample_metadata(NICKNAME):
     return jsonify(sample_metadata)
 
 
-# @app.route("/samples/<sample>")
-# def samples(sample):
-#     """Return `otu_ids`, `otu_labels`,and `sample_values`."""
-#     stmt = db.session.query(Samples).statement
-#     df = pd.read_sql_query(stmt, db.session.bind)
+@app.route("/games")
+def samples():
+    """Return the game data."""
+    sample_data = [
+        Samples.GAME_ID,
+        Samples.HOME_TEAM_ID,
+        Samples.VISITOR_TEAM_ID,
+        Samples.PTS_home,
+        Samples.FG_PCT_home,
+        Samples.FT_PCT_home,
+        Samples.FG3_PCT_home,
+        Samples.AST_home,
+        Samples.REB_home,
+        Samples.PTS_away,
+        Samples.FG_PCT_away,
+        Samples.FT_PCT_away,
+        Samples.FG3_PCT_away,
+        Samples.AST_away,
+        Samples.REB_away,
+        Samples.HOME_TEAM_WINS
+    ]
 
-#     # Filter the data based on the sample number and
-#     # only keep rows with values above 1
-#     sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
+    # Query the database for the games data
+    results = db.session.query(*sample_data).all()
 
-#     # Sort by sample
-#     sample_data.sort_values(by=sample, ascending=False, inplace=True)
-
-#     # Format the data to send as json
-#     data = {
-#         "otu_ids": sample_data.otu_id.values.tolist(),
-#         "sample_values": sample_data[sample].values.tolist(),
-#         "otu_labels": sample_data.otu_label.tolist(),
-#     }
-#     return jsonify(data)
+    # Format the data to send as json
+    game_data = []
+    home_team = {}
+    for game_id, home_team_id, visitor_team, pts_home, fg_pct_home, ft_pct_home, fg3_pct_home, ast_home, reb_home,  pts_away, fg_pct_away, ft_pct_away, fg3_pct_away, ast_away, reb_away, home_wins in results:
+        home_team["GAME_ID"] = game_id
+        home_team["HOME_TEAM_ID"] = home_team_id
+        home_team["VISITOR_TEAM_ID"] = visitor_team
+        home_team["PTS_home"] = pts_home
+        home_team["FG_PCT_home"] = fg_pct_home
+        home_team["FT_PCT_home"] = ft_pct_home
+        home_team["FG3_PCT_home"] = fg3_pct_home
+        home_team["AST_home"] = ast_home
+        home_team["REB_home"] = reb_home
+        home_team["PTS_away"] = pts_away
+        home_team["FG_PCT_away"] = fg_pct_away
+        home_team["FT_PCT_away"] = ft_pct_away
+        home_team["FG3_PCT_away"] = fg3_pct_away
+        home_team["AST_away"] = ast_away
+        home_team["REB_away"] = reb_away
+        home_team["HOME_TEAM_WINS"] = home_wins
+        game_data.append(home_team)
+    print(f'......>>>>>>>>>>>>>>>>.......This is the home team: {home_team}')
+    return jsonify(game_data)
 
 
 if __name__ == "__main__":
