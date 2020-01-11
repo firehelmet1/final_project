3dd1b9ce8ab01878f5ee5bfcbef9d1082db2c5b1
Michael Pearson <mpearson2516@yahoo.com>
2020-01-11 13:58:17 -0800

    Made the prediction model into a callable function.

--- Frontend/nba_games/games_predict_single.py
+++ Frontend/nba_games/games_predict_single.py
@@ -1,63 +1,86 @@
 #Import Dependencies
 import numpy as np
+import requests
 import pandas as pd
 from datetime import datetime, date
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from flask import jsonify
 
-## Read in Games, Teams, Matchups datasets
-game = pd.read_csv('games.csv')
-teams = pd.read_csv('teams.csv')
+## Read in Games and Teams api 
+game_data = requests.get('/games').json()
+team_data = requests.get('/metadata/<NICKNAME>').json()
 
-#Input Home, Visitor Teams  
-home = input("Enter Home Team Abbreviation: ") 
-visitor = input("Enter Visitor Team Abbreviation: ")
+# Use Pandas to read json data
+game = pd.read_json(game_data)
+teams = pd.read_json(team_data)
 
-for i in range(len(teams)):
-    if teams.iloc[i]['ABBREVIATION'] == home:
-        home_id = teams.iloc[i]['TEAM_ID']
+# Build a function that will run the prediction model
+def prediction_model(home, away):
 
-    if teams.iloc[i]['ABBREVIATION'] == visitor:
-        visitor_id = teams.iloc[i]['TEAM_ID']
+    #Input Home, Visitor Teams  
+    home = home
+    visitor = away
 
-#Filter games to matched home, visitor teams
-game = game[game['HOME_TEAM_ID'] == home_id]
-game = game[game['VISITOR_TEAM_ID'] == visitor_id]
+    for i in range(len(teams)):
+        if teams.iloc[i]['ABBREVIATION'] == home:
+            home_id = teams.iloc[i]['TEAM_ID']
+
+        if teams.iloc[i]['ABBREVIATION'] == visitor:
+            visitor_id = teams.iloc[i]['TEAM_ID']
+
+    #Filter games to matched home, visitor teams
+    games = game[game['HOME_TEAM_ID'] == home_id]
+    games = games[games['VISITOR_TEAM_ID'] == visitor_id]
     
-#Drop dates, team_ids, game_ids, season - will not be useful
-game.drop(['GAME_ID','HOME_TEAM_ID', 
-            'VISITOR_TEAM_ID', 'SEASON'], axis=1)
-game.dropna(inplace=True)
+    #Drop dates, team_ids, game_ids, season - will not be useful
+    game.drop(['GAME_ID','HOME_TEAM_ID', 
+                'VISITOR_TEAM_ID'], axis=1)
+    game.dropna(inplace=True)
     
-#Split Dataset for training, test
-y = game["HOME_TEAM_WINS"]
-X = game.drop("HOME_TEAM_WINS", axis=1)
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, stratify=y)
+    #Split Dataset for training, test
+    y = game["HOME_TEAM_WINS"]
+    X = game.drop("HOME_TEAM_WINS", axis=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, stratify=y)
 
-#Calculate Historical Win%
-wins = 0
-for j in range(len(y)):
-    wins = wins + round(y.iloc[j])
-record = wins / len(y)    
+    #Calculate Historical Win%
+    wins = 0
+    for j in range(len(y)):
+        wins = wins + round(y.iloc[j])
+    record = wins / len(y)    
 
-#Random Forest Classification
-rf = RandomForestClassifier(n_estimators=128)
-rf = rf.fit(X_train, y_train)
+    #Random Forest Classification
+    rf = RandomForestClassifier(n_estimators=128)
+    rf = rf.fit(X_train, y_train)
     
-# Fitting our model with all of our features in X
-score = rf.score(X, y)
-predictor=(rf.predict(X).mean()- 0.5*np.var(rf.predict(X))/record)
+    # Fitting our model with all of our features in X
+    score = rf.score(X_test, y_test)
+    predictor=(rf.predict(X).mean()- 0.5*np.var(rf.predict(X))/record)
 
-# Calculate Spread
-home_score = game['PTS_home'].mean()
-visitor_score = game['PTS_away'].mean()
-spread = home_score - visitor_score
+    # Calculate Spread
+    home_score = game['PTS_home'].mean()
+    visitor_score = game['PTS_away'].mean()
+    spread = home_score - visitor_score
 
-# Predict
-if predictor > 0.5:
-    win_predictor = home
-else:
-    win_predictor = visitor
+    # Predict
+    if predictor > 0.5:
+        if spread >= 3:
+            win_predictor = home
+        else:
+            win_predictor = visitor
+    else:
+        win_predictor = visitor
             
-#game prediction accuracy
-print(win_predictor, score, spread)
+    #game prediction accuracy
+    print(win_predictor, score, spread)
+    feature_weights = (sorted(zip(rf.feature_importances_, X.columns), reverse = True))
+    
+    # Put the prediction, score, spread, and feature weights into a dictionary
+    predict_dict = {}
+    predict_dict["prediction"] = win_predictor
+    predict_dict["score"] = score
+    predict_dict["spread"] = spread
+    predict_dict["weights"] = feature_weights
+
+    # Return a jsonified version of the dictionary
+    return jsonify(predict_dict)
